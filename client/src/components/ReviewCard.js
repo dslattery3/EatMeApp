c1bc22d6aa4c3982b95e2b51d72160f9e744b88e
Shane <MalithSM@hotmail.com>
2022-03-29 09:56:26 -0500

    hungry monster

--- client/src/components/ReviewCard.js
+++ client/src/components/ReviewCard.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
 // created_at: "2022-03-28T22:02:42.598Z"
 // description: "love it"
@@ -7,27 +7,41 @@ import React, {useState} from 'react'
 // updated_at: "2022-03-28T22:02:42.598Z"
 // user: "a"
 
-function ReviewCard({r}) {
-  const [isEdited, setIsEdited] = useState(false)
+function ReviewCard({ r }) {
+  const [isEdited, setIsEdited] = useState(false);
 
-  if (r?.created_at !== r?.updated_at){
-    setIsEdited(true)
+  if (r?.created_at !== r?.updated_at) {
+    setIsEdited(true);
   }
   //find a better way to format date
-  const reviewDate = (r.created_at.substring(5,8) + r.created_at.substring(8,10) + '-' + r.created_at.substring(0,4))
-  const reviewUpdate = (r.updated_at.substring(5,8) + r.updated_at.substring(8,10) + '-' + r.updated_at.substring(0,4))
+  const reviewDate =
+    r.created_at.substring(5, 8) +
+    r.created_at.substring(8, 10) +
+    "-" +
+    r.created_at.substring(0, 4);
+  const reviewUpdate =
+    r.updated_at.substring(5, 8) +
+    r.updated_at.substring(8, 10) +
+    "-" +
+    r.updated_at.substring(0, 4);
 
   return (
-    <div className='review-card'>
-        <h4>Recipe Name</h4>
-        <div>
-          <img src={r.image_url} alt={r.name}/>
-        </div>
-        <h4>{r.description}</h4>
-        <p>Created: {reviewDate}</p>
-        {isEdited && <p>Edited: {`${reviewUpdate}`}</p>}
+    <div className="review-card">
+      <h4>{r.recipe}</h4>
+      <div>
+        <img src={r.image_url} alt={r.name} />
+      </div>
+      <h4>{r.description}</h4>
+      <p>
+        <img
+          src="https://res.cloudinary.com/dtglqdhwm/image/upload/v1648565391/hungry1_m7dpqz.png"
+          alt="hungryReview"
+        />
+        Created: {reviewDate}
+      </p>
+      {isEdited && <p>Edited: {`${reviewUpdate}`}</p>}
     </div>
-  )
+  );
 }
 
-export default ReviewCard
+export default ReviewCard;
--- client/src/components/UserPage.js
+++ client/src/components/UserPage.js
@@ -1,23 +1,25 @@
-import React from 'react'
-import RecipeCard from './RecipeCard'
-import ReviewCard from './ReviewCard'
+import React from "react";
+import RecipeCard from "./RecipeCard";
+import ReviewCard from "./ReviewCard";
 
-function UserPage({user}) {
-  console.log(user)
-  // const userRecipes = user.recipes.map(r => <RecipeCard key={r.id} r={r} fromUser={true}/>)
-  const userReviews = user.reviews.map(r => <ReviewCard key={r.id} r={r} />)
+function UserPage({ user }) {
+  // console.log(user)
+  // const userRecipes = user.recipes.map((r) => (
+  //   <RecipeCard key={r.id} r={r} fromUser={true} />
+  // ));
+  const userReviews = user.reviews.map((r) => <ReviewCard key={r.id} r={r} />);
+
   return (
     <div>
-      <h1 align='center'>{`${user.username}'s Page`}</h1>
-      <div className='container'>
-        {/* {userRecipes} */}
-      </div>
-      <h3 align='center' className='user-reviews'>{`${user.username}'s Reviews`}</h3>     
-      <div className='review-container'>
-        {userReviews}
-      </div>
+      <h1 align="center">{`${user.username}'s Page`}</h1>
+      {/* <div className="container">{userRecipes}</div> */}
+      <h3
+        align="center"
+        className="user-reviews"
+      >{`${user.username}'s Reviews`}</h3>
+      <div className="container">{userReviews}</div>
     </div>
-  )
+  );
 }
 
-export default UserPage
+export default UserPage;
