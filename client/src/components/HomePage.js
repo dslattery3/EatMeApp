6e53424fb834a2db2354a48adc35e0e087b40167
Shane <MalithSM@hotmail.com>
2022-03-30 08:56:27 -0500

    cookie monster

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,30 +1,35 @@
-import React from 'react'
-import {NavLink} from 'react-router-dom'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
-function HomePage({user}) {
+function HomePage({ user }) {
   return (
-    <div align ='center' className='homepage'>
+    <div align="center" className="homepage">
       <h1> EatMe </h1>
-      <div className='home-container'>
-        <img src={require('../images/EatME1.jpg')} alt='eatme logo'/>
+      <div className="home-container">
+        <img src={require("../images/EatME1.jpg")} alt="eatme logo" />
         <p>
-          Welcome to EatMe where eating more is always encouraged. In order to view our recipes from our catalog you will need to login or register. Once you're signed in, you'll be able to feed the beast inside.
+          Welcome to EatMe where eating more is always encouraged. In order to
+          view our recipes from our catalog you will need to login or register.
+          Once you're signed in, you'll be able to feed the beast inside.
         </p>
+        <img src={require("../images/cookie.gif")} alt="cookie monster" />
       </div>
-      {!user && <div className='signup-login-container'>
-      <NavLink to='/signup'>
-        <button> SignUp </button>
-      </NavLink>
-      <NavLink to='/login'>
-        <button> Login </button>
-      </NavLink>
-      </div>}
-      <div className='top-recipe-container'>
+      {!user && (
+        <div className="signup-login-container">
+          <NavLink to="/signup">
+            <button> SignUp </button>
+          </NavLink>
+          <NavLink to="/login">
+            <button> Login </button>
+          </NavLink>
+        </div>
+      )}
+      <div className="top-recipe-container">
         <h3>Top Rated Recipes</h3>
         Top Recipe Container
       </div>
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
